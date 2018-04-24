7b426901067a31abe8cf7bbff1c7191865301968
sbodapat <38225726+sbodapat@users.noreply.github.com>
2018-04-24 22:18:07 +0530

    Added new lines of code to support switch functionality. - Added the pin numbers to connect the switch in Instructions.txt

--- ad_player.py
+++ ad_player.py
@@ -8,10 +8,12 @@ import time
 import Image
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11,GPIO.OUT)	#Used to run the IR LED
-GPIO.setup(7,GPIO.IN) #Setting Pin 7 as input
 
-GPIO.output(11, TRUE) #Switching the IR LED ON.
+# GPIO 17 is set up as input, pulled up to avoid false detection.
+# The Port is wired to connect to GND on button press.  
+# So we'll be setting up falling edge detection
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 path ='/media/usb/'
 
 def displayThanks(channel):
@@ -20,11 +22,13 @@ def displayThanks(channel):
 	image=Image.open(os.path.join(path, 'Thankyou.jpg'))
 	image.show()
 	time.sleep(5)
+	#Take input from the numeric keypad
+	raw_input("Enter your number from the keypad\n>")
 	#Do the printer job over here.
 	#Print job command starts over here.
-	os.startfile("print.docx","print")
+	#os.startfile("print.docx","print")
 
 while(1):
 	for infile in glob.glob(os.path.join(path, '*.mp4')):
-	  a = subprocess.call( [ "omxplayer", "-o", "hdmi", infile])
-	  GPIO.add_event_detect(7, GPIO.RISING, callback=displayThanks, bouncetime=5)
+		a = subprocess.call( [ "omxplayer", "-o", "hdmi", infile])
+		GPIO.add_event_detect(17, GPIO.FALLING, callback=displayThanks, bouncetime=350)
